37f8192795bd8d923916169cd3ddbce1590431d2
Jormin <jorminxee@gmail.com>
2021-08-11 20:53:48 +0800

    change

--- kafka/entrypoint.sh
+++ kafka/entrypoint.sh
@@ -23,13 +23,12 @@ then
 	then
 		sed -i -e "s/localhost:2181/$ZOOKEEPER_SERVERS/" /usr/local/share/kafka/config/server.properties
 	fi
+fi
 
-	if [[ $JMX_REMOTE_RMI_PORT ]]
-	then
-		sed -i "/export KAFKA_HEAP_OPTS=/a\    export JMX_PORT=\"$JMX_REMOTE_RMI_PORT\"" /usr/local/share/kafka/bin/kafka-server-start.sh
-		sed -i -e 's/^.*KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremot.*$/  KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false  -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port='"$JMX_REMOTE_RMI_PORT"' -Djava.rmi.server.hostname='"$JMX_REMOTE_RMI_SERVER_HOSTNAME"' -Dcom.sun.management.jmxremote.local.only=false "/' /usr/local/share/kafka/bin/kafka-run-class.sh
-		sed -i -e "s/localhost:2181/$ZOOKEEPER_SERVERS/" /usr/local/share/kafka/config/server.properties
-	fi
+if [[ $JMX_REMOTE_RMI_PORT ]]
+then
+	sed -i "/export KAFKA_HEAP_OPTS=/a\    export JMX_PORT=\"$JMX_REMOTE_RMI_PORT\"" /usr/local/share/kafka/bin/kafka-server-start.sh
+	sed -i -e 's/^.*KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremot.*$/  KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false  -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.rmi.port='"$JMX_REMOTE_RMI_PORT"' -Djava.rmi.server.hostname='"$JMX_REMOTE_RMI_SERVER_HOSTNAME"' -Dcom.sun.management.jmxremote.local.only=false "/' /usr/local/share/kafka/bin/kafka-run-class.sh
 fi
 
 # tail -f / dev / null
